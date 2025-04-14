764f78bb3db00cccd940b5b54a7c4aeff7922c3c
Victor Soffi <victorsoffi@gmail.com>
2025-04-14 00:02:30 -0300

    aplicação rodando + docker setup http res ok

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,55 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"gateway/internal/repository"
+	"gateway/internal/service"
+	"gateway/internal/web/server"
+	"log"
+	"os"
 
-func main(){
-	println("Hello World")
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func getEnv(key, defaultValue string) string{
+	if value := os.Getenv(key); value != ""{
+		return value
+	}
+	return defaultValue
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Erro loading .env file")
+	}
+	//string de conexao
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		getEnv("DB_HOST", "db"),
+		getEnv("DB_PORT", "5432"),
+		getEnv("DB_USER", "postgres"),
+		getEnv("DB_PASSWORD", "postgres"),
+		getEnv("DB_NAME", "gateway"),
+		getEnv("DB_SSL_MODE", "disable"),
+	)
+	//conexao com db
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Erro connecting to database: ", err)
+	}
+	defer db.Close()
+
+	accountRepository := repository.NewAccountRepository(db)
+	accountService := service.NewAccountService(accountRepository)
+
+	port := getEnv("HTTP_PORT", "8080")
+	srv := server.NewServer(accountService, port)
+	srv.ConfigureRoutes()
+
+	srv.Start()
+	if err != nil{
+		log.Fatal("Error starting server: ", err)
+	}
 }
